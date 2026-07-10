8b482537e98a7ab6028fb11a9cf911b2898344d0
Tejas <tejas@example.com>
2026-07-10 13:42:20 +0530

    Fix Vercel build compilation: define exact relative module declarations for Relume components

--- client/src/declarations.d.ts
+++ client/src/declarations.d.ts
@@ -1,18 +1,23 @@
-declare module '*Navbar9' {
+declare module '../home/components/Navbar9' {
   export const Navbar9: any;
 }
-declare module '*Header83' {
+declare module '../home/components/Header83' {
   export const Header83: any;
 }
-declare module '*Layout504' {
+declare module '../home/components/Layout504' {
   export const Layout504: any;
 }
-declare module '*Layout503' {
+declare module '../home/components/Layout503' {
   export const Layout503: any;
 }
-declare module '*Cta25' {
+declare module '../home/components/Cta25' {
   export const Cta25: any;
 }
-declare module '*Footer7' {
+declare module '../home/components/Footer7' {
   export const Footer7: any;
+}
+
+declare module "*.jsx" {
+  const content: any;
+  export default content;
 }
