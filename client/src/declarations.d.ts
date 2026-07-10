b48994efb7495d926426103e07d8e68afe51bf87
Tejas <tejas@example.com>
2026-07-10 13:38:48 +0530

    Fix Vercel build compilation: Add declarations.d.ts to define JSX component modules

---
+++ client/src/declarations.d.ts
@@ -0,0 +1,18 @@
+declare module '*Navbar9' {
+  export const Navbar9: any;
+}
+declare module '*Header83' {
+  export const Header83: any;
+}
+declare module '*Layout504' {
+  export const Layout504: any;
+}
+declare module '*Layout503' {
+  export const Layout503: any;
+}
+declare module '*Cta25' {
+  export const Cta25: any;
+}
+declare module '*Footer7' {
+  export const Footer7: any;
+}
